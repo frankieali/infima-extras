345bdd5fd02fdc94ceb4ed7ae8f2b29f94b42e06
frankieali <frankieali4@gmail.com>
2024-06-06 15:13:52 -0400

    update configs

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -23,6 +23,8 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'frankieali', // Usually your GitHub org/user name.
   projectName: 'infima-inputs', // Usually your repo name.
+  deploymentBranch: "gh-pages",
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
